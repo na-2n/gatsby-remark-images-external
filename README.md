0c981b46a2bc5abf5029b0c4d7a58f648601d9d5
yuwui <uwu@yui.pet>
2020-07-26 20:31:16 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# gatsby-remark-images-external
+
+"Fork" of [gatsby-remark-images](https://github.com/gatsbyjs/gatsby/blob/master/packages/gatsby-remark-images/) with support for external files.
+
---
+++ src/constants.js
@@ -0,0 +1,17 @@
+export const DEFAULT_OPTIONS = {
+    maxWidth: 650,
+    wrapperStyle: "",
+    backgroundColor: "white",
+    linkImagesToOriginal: true,
+    showCaptions: false,
+    markdownCaptions: false,
+    withWebp: false,
+    tracedSVG: false,
+    loading: "lazy",
+    disableBgImageOnAlpha: false,
+    disableBgImage: false,
+};
+
+export const imageClass = "gatsby-resp-image-image";
+export const imageWrapperClass = "gatsby-resp-image-wrapper";
+export const imageBackgroundClass = "gatsby-resp-image-background-image";
